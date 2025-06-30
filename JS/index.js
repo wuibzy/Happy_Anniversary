ffe5fef8d16c9e94382254c08c0b18de59799ab4
wuibzy <138027055+wuibzy@users.noreply.github.com>
2025-06-30 22:40:14 +0800

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -2,7 +2,7 @@ function Password (){
     const password = parseFloat(document.getElementById("passnumber").value);
 
     if (password === 21434) {
-        window.location.href = "../home.html";
+        window.location.href = "../Happy_Anniversary/home.html";
     } else {
         alert("Wrong Password, try again")
     }
